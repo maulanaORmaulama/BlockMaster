aa17de27aba4409338bed89646ac1d857f682e0d
maulanaORmaulama <maulanaisnaandika@gmail.com>
2026-06-13 13:56:24 +0800

    ini buat projectnya

---
+++ BlokMaster.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <conio.h>
+#include <chrono>
+#include <thread>
+#include <string>
+
+//warna
+#define RESET   "\033[0m"
+#define GREEN   "\033[1;32m"
+#define YELLOW  "\033[1;33m"
+#define RED     "\033[1;31m"
+#define ORANGE  "\033[38;2;255;165;0m"
+#define PURPLE  "\033[38;2;128;0;128m"
+
+
+int main(){
+
+}
